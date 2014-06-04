d902f7ff1502182f4912bb24876d96540de0e805
Christoph Reiter <reiter.christoph@gmail.com>
2014-06-04 14:28:51 +0200

    disable debug again

--- web/index.py
+++ web/index.py
@@ -107,7 +107,7 @@ Disallow: /
 
 if __name__ == '__main__':
 
-    app.run(host='0.0.0.0', port=80, use_reloader=False, debug=True)
+    #app.run(host='0.0.0.0', port=80, use_reloader=False, debug=True)
 
     from tornado.wsgi import WSGIContainer
     from tornado.httpserver import HTTPServer
