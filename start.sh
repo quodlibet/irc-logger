a54687faf59f65f35dc1c65a078a83f2421c4325
Christoph Reiter <reiter.christoph@gmail.com>
2014-10-16 00:12:31 +0200

    update+

--- start.sh
+++ start.sh
@@ -10,16 +10,16 @@ SLAVE="$BUILDBOT/slave"
 buildbot restart "$MASTER"
 buildslave restart "$SLAVE"
 
-# duckdns updater
+# freedns updater
 
-DUCKDNSPID="$DIR/.duckdns.pid"
-if [ -f "$DUCKDNSPID" ]; then
-    pkill -P $(cat "$DUCKDNSPID")
-    kill $(cat "$DUCKDNSPID")
-    rm "$DUCKDNSPID"
+FREEDNSPID="$DIR/.freedns.pid"
+if [ -f "$FREEDNSPID" ]; then
+    pkill -P $(cat "$FREEDNSPID")
+    kill $(cat "$FREEDNSPID")
+    rm "$FREEDNSPID"
 fi
-./duckdns-update.sh >/dev/null 2>&1 &
-echo $! > "$DUCKDNSPID"
+./freedns-update.sh >/dev/null 2>&1 &
+echo $! > "$FREEDNSPID"
 
 # google bot
 
