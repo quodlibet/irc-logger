3c4346fedf509522884248fc6da73f07eec4b223
Christoph Reiter <reiter.christoph@gmail.com>
2015-06-02 18:36:47 +0200

    remove misc

--- reconfig.sh
+++ reconfig.sh
@@ -2,16 +2,9 @@
 
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
-# buildbot
-
 BUILDBOT="$DIR/buildbot"
 MASTER="$BUILDBOT/master"
-"$DIR/config.py" "$DIR/misc/buildbot.cfg.tmpl" > "$MASTER/master.cfg"
-
-# google bot
-
-GOOGLEBOT="$DIR/googlebot"
-"$DIR/config.py" "$DIR/misc/googlebot.yaml.tmpl" > "$GOOGLEBOT/bot.yaml"
+"$DIR/config.py" "$DIR/buildbot.cfg.tmpl" > "$MASTER/master.cfg"
 
 echo "done."
 echo "Call start.sh to restart all services."
