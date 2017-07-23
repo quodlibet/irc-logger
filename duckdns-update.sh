5a78d654ad8aa406baf11967516e01628420415a
Christoph Reiter <reiter.christoph@gmail.com>
2017-07-23 23:47:55 +0200

    fix duckdns, they self sign now

--- duckdns-update.sh
+++ duckdns-update.sh
@@ -5,6 +5,6 @@ TOKEN=$("$DIR/config.py" DUCKDNS_TOKEN)
 DOMAIN=$("$DIR/config.py" DUCKDNS_DOMAIN)
 
 while true; do
-    echo url="https://www.duckdns.org/update?domains=$DOMAIN&token=$TOKEN&ip=" | curl --config -
+    echo url="https://www.duckdns.org/update?domains=$DOMAIN&token=$TOKEN&ip=" | curl --insecure --config -
     sleep 300
 done
