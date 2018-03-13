1deab3187b86ee2b394b50b484bde7dde906472e
Christoph Reiter <reiter.christoph@gmail.com>
2018-03-13 09:19:36 +0100

    basic logging..

--- start.sh
+++ start.sh
@@ -2,6 +2,10 @@
 
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
+LOGS="$DIR/_logs"
+
+mkdir -p "$LOGS"
+
 # duckdns updater
 
 DUCKDNSPID="$DIR/.duckdns.pid"
@@ -10,7 +14,7 @@ if [ -f "$DUCKDNSPID" ]; then
     kill $(cat "$DUCKDNSPID")
     rm "$DUCKDNSPID"
 fi
-./duckdns-update.sh >/dev/null 2>&1 &
+./duckdns-update.sh >"$LOGS/duckdns.log" 2>&1 &
 echo $! > "$DUCKDNSPID"
 
 # web server
@@ -22,5 +26,5 @@ if [ -f "$WEBPID" ]; then
     kill $(cat "$WEBPID")
     rm "$WEBPID"
 fi
-authbind python3 "$WEBAPP" 80 443 --irc >/dev/null 2>&1 &
+authbind python3 "$WEBAPP" 80 443 --irc >"$LOGS/web.log" 2>&1 &
 echo $! > "$WEBPID"
