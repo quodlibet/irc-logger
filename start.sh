5aa4941e545e44e648e158673e8d66900111b67a
Christoph Reiter <reiter.christoph@gmail.com>
2018-05-11 18:45:21 +0200

    Revert "basic logging.."
    This reverts commit 1deab3187b86ee2b394b50b484bde7dde906472e.

--- start.sh
+++ start.sh
@@ -2,10 +2,6 @@
 
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
-LOGS="$DIR/_logs"
-
-mkdir -p "$LOGS"
-
 # duckdns updater
 
 DUCKDNSPID="$DIR/.duckdns.pid"
@@ -14,7 +10,7 @@ if [ -f "$DUCKDNSPID" ]; then
     kill $(cat "$DUCKDNSPID")
     rm "$DUCKDNSPID"
 fi
-./duckdns-update.sh >"$LOGS/duckdns.log" 2>&1 &
+./duckdns-update.sh >/dev/null 2>&1 &
 echo $! > "$DUCKDNSPID"
 
 # web server
@@ -26,5 +22,5 @@ if [ -f "$WEBPID" ]; then
     kill $(cat "$WEBPID")
     rm "$WEBPID"
 fi
-authbind python3 "$WEBAPP" 80 443 --irc >"$LOGS/web.log" 2>&1 &
+authbind python3 "$WEBAPP" 80 443 --irc >/dev/null 2>&1 &
 echo $! > "$WEBPID"
